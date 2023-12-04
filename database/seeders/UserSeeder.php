c3a06988add6aa0531e557c8239d4a3a000a2192
arifjagad <arifjagad34@gmail.com>
2023-12-04 21:55:20 +0700

    Install Fortify and initial configuration

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        User::factory(5)->create();
+
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('admin')
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,25 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.blank-page', ['type_menu' => '']);
-});
-
-Route::get('/login', function () {
     return view('auth.login', ['type_menu' => '']);
 });
 
-Route::get('/register', function () {
-    return view('auth.register', ['type_menu' => '']);
-});
-
-Route::get('/forgot', function () {
-    return view('auth.forgot', ['type_menu' => '']);
-});
-
-Route::get('/reset', function () {
-    return view('auth.reset', ['type_menu' => '']);
-});
-
-Route::get('/verify', function () {
-    return view('auth.verify', ['type_menu' => '']);
+Route::middleware(['auth'])->group(function(){
+    Route::get('dashboard', function () {
+        return view('pages.dashboard', ['type_menu' => 'dashboard']);
+    });
 });
