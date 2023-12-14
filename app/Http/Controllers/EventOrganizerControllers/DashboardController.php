1c5e754977269c2a871d3443ce3b8dab3ec24580
arifjagad <arifjagad34@gmail.com>
2023-12-15 03:12:15 +0700

    Fixing Profile Photo Bug on Dashboard.

--- app/Http/Controllers/EventOrganizerControllers/DashboardController.php
+++ app/Http/Controllers/EventOrganizerControllers/DashboardController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers\EventOrganizerControllers;
 
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use App\Models\EventOrganizer;
 
 class DashboardController extends Controller
 {
     public function indexDashboard_EventOrganizer(){
-        return view('event_organizer.dashboard-eo', ['type_menu' => 'event_organizer.dashboard-eo'],);
+        $data = EventOrganizer::where('id_user', auth()->id())->first();
+
+        return view('event_organizer.dashboard-eo', ['type_menu' => 'event_organizer.dashboard-eo'], compact('data'));
     }
 }
