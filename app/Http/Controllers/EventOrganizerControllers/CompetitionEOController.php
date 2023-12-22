af0b5b54e1a9c65f90cd1ec87f362fde04af981c
arifjagad <arifjagad34@gmail.com>
2023-12-23 00:59:58 +0700

    Installing HTMLPurifier and Updating Description Upload Functionality (2).

--- app/Http/Controllers/EventOrganizerControllers/CompetitionEOController.php
+++ app/Http/Controllers/EventOrganizerControllers/CompetitionEOController.php
@@ -10,6 +10,9 @@ use Illuminate\Http\Request;
 use DOMDocument;
 use Illuminate\Support\Facades\Auth;
 use App\Models\DetailEvent;
+use HTMLPurifier_Config;
+use HTMLPurifier;
+
 class CompetitionEOController extends Controller
 {
     public function indexCompetitionEO(){
@@ -59,20 +62,27 @@ class CompetitionEOController extends Controller
 
             // Upload data dari summernote untuk deskripsi event
             $competition_description = $request->competition_description;
+
+            $config = HTMLPurifier_Config::createDefault();
+            $purifier = new HTMLPurifier($config);
+            $competition_description = $purifier->purify($competition_description);
+
             $dom = new DOMDocument();
             if (!empty($competition_description)) {
-                $dom->loadHTML($competition_description, 9);
+                $dom->loadHTML($competition_description, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
             }
 
             $images = $dom->getElementsByTagName('img');
 
             foreach ($images as $key => $img) {
-                $data = base64_decode(explode(',',explode(';',$img->getAttribute('src'))[1])[1]);
-                $image_name = "/upload/" . time(). $key.'.png';
-                file_put_contents(public_path().$image_name,$data);
+                if (strpos($img->getAttribute('src'),'data:image/') === 0) {
+                    $data = base64_decode(explode(',', explode(';', $img->getAttribute('src'))[1])[1]);
+                    $image_name = "/upload/" . time() . $key . '.png';
+                    file_put_contents(public_path() . $image_name, $data);
 
-                $img->removeAttribute('src');
-                $img->setAttribute('src',$image_name);
+                    $img->removeAttribute('src');
+                    $img->setAttribute('src', $image_name);
+                }
             }
 
             $competition_description = $dom->saveHTML();
@@ -151,20 +161,27 @@ class CompetitionEOController extends Controller
 
             // Upload data dari summernote untuk deskripsi event
             $competition_description = $request->competition_description;
+
+            $config = HTMLPurifier_Config::createDefault();
+            $purifier = new HTMLPurifier($config);
+            $competition_description = $purifier->purify($competition_description);
+
             $dom = new DOMDocument();
             if (!empty($competition_description)) {
-                $dom->loadHTML($competition_description, 9);
+                $dom->loadHTML($competition_description, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
             }
 
             $images = $dom->getElementsByTagName('img');
 
             foreach ($images as $key => $img) {
-                $data = base64_decode(explode(',',explode(';',$img->getAttribute('src'))[1])[1]);
-                $image_name = "/upload/" . time(). $key.'.png';
-                file_put_contents(public_path().$image_name,$data);
+                if (strpos($img->getAttribute('src'),'data:image/') === 0) {
+                    $data = base64_decode(explode(',', explode(';', $img->getAttribute('src'))[1])[1]);
+                    $image_name = "/upload/" . time() . $key . '.png';
+                    file_put_contents(public_path() . $image_name, $data);
 
-                $img->removeAttribute('src');
-                $img->setAttribute('src',$image_name);
+                    $img->removeAttribute('src');
+                    $img->setAttribute('src', $image_name);
+                }
             }
 
             $competition_description = $dom->saveHTML();
--- app/Http/Controllers/EventOrganizerControllers/EventEOController.php
+++ app/Http/Controllers/EventOrganizerControllers/EventEOController.php
@@ -63,20 +63,27 @@ class EventEOController extends Controller
             
             // Upload data dari summernote untuk deskripsi event
             $event_description = $request->event_description;
+
+            $config = HTMLPurifier_Config::createDefault();
+            $purifier = new HTMLPurifier($config);
+            $event_description = $purifier->purify($event_description);
+
             $dom = new DOMDocument();
             if (!empty($event_description)) {
-                $dom->loadHTML($event_description, 9);
+                $dom->loadHTML($event_description, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
             }
 
             $images = $dom->getElementsByTagName('img');
 
             foreach ($images as $key => $img) {
-                $data = base64_decode(explode(',',explode(';',$img->getAttribute('src'))[1])[1]);
-                $image_name = "/upload/" . time(). $key.'.png';
-                file_put_contents(public_path().$image_name,$data);
+                if (strpos($img->getAttribute('src'),'data:image/') === 0) {
+                    $data = base64_decode(explode(',', explode(';', $img->getAttribute('src'))[1])[1]);
+                    $image_name = "/upload/" . time() . $key . '.png';
+                    file_put_contents(public_path() . $image_name, $data);
 
-                $img->removeAttribute('src');
-                $img->setAttribute('src',$image_name);
+                    $img->removeAttribute('src');
+                    $img->setAttribute('src', $image_name);
+                }
             }
 
             $event_description = $dom->saveHTML();
@@ -152,35 +159,31 @@ class EventEOController extends Controller
             ]);
 
             // Upload data dari summernote untuk deskripsi event
-$event_description = $request->event_description;
+            $event_description = $request->event_description;
 
-// Clean and validate HTML using HtmlPurifier
-$config = HTMLPurifier_Config::createDefault();
-// Add the import statement for HTMLPurifier
-// Instantiate the HTMLPurifier class
-$purifier = new HTMLPurifier($config);
-$event_description = $purifier->purify($event_description);
+            $config = HTMLPurifier_Config::createDefault();
+            $purifier = new HTMLPurifier($config);
+            $event_description = $purifier->purify($event_description);
 
-$dom = new DOMDocument();
-if (!empty($event_description)) {
-    // Load cleaned and validated HTML
-    $dom->loadHTML($event_description, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
-}
+            $dom = new DOMDocument();
+            if (!empty($event_description)) {
+                $dom->loadHTML($event_description, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+            }
 
-$images = $dom->getElementsByTagName('img');
+            $images = $dom->getElementsByTagName('img');
 
-foreach ($images as $key => $img) {
-    if (strpos($img->getAttribute('src'),'data:image/') === 0) {
-        $data = base64_decode(explode(',', explode(';', $img->getAttribute('src'))[1])[1]);
-        $image_name = "/upload/" . time() . $key . '.png';
-        file_put_contents(public_path() . $image_name, $data);
+            foreach ($images as $key => $img) {
+                if (strpos($img->getAttribute('src'),'data:image/') === 0) {
+                    $data = base64_decode(explode(',', explode(';', $img->getAttribute('src'))[1])[1]);
+                    $image_name = "/upload/" . time() . $key . '.png';
+                    file_put_contents(public_path() . $image_name, $data);
 
-        $img->removeAttribute('src');
-        $img->setAttribute('src', $image_name);
-    }
-}
+                    $img->removeAttribute('src');
+                    $img->setAttribute('src', $image_name);
+                }
+            }
 
-$event_description = $dom->saveHTML();
+            $event_description = $dom->saveHTML();
 
 
             // Upload featured image
